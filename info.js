76833bbaf4b8e416f63a4cc6fc9286adf1d06b50
cy <ysabudikey@gmail.com>
2026-06-01 16:13:26 +0800

    Add files via upload

---
+++ info.js
@@ -0,0 +1,13 @@
+const creds = [
+  { u: btoa("prellegera"), p: btoa("s5143!"), target: "s5.html" }, //sunny
+  { u: btoa("flores"), p: btoa("l5143!"), target: "l5.html" }, //lanah
+  { u: btoa("gonzales"), p: btoa("a4143!"), target: "a4.html" }, //adri
+  { u: btoa("santiago"), p: btoa("j3143!"), target: "j3.html" }, //jaz
+  { u: btoa("carbungco"), p: btoa("o4143!"), target: "o4.html" }, //ozno
+  { u: btoa("soriano"), p: btoa("a6143!"), target: "a6.html" }, //audrey
+  { u: btoa("lizares"), p: btoa("m5143!"), target: "m5.html" }, //marie
+  { u: btoa("pagalilauan"), p: btoa("j6143!"), target: "j6.html" }, //jae
+  { u: btoa("kua"), p: btoa("m3143!"), target: "m3.html" }, //mir
+  { u: btoa("gatus"), p: btoa("c5143!"), target: "mc5.html" }, //cess
+  { u: btoa("austin"), p: btoa("s6143!"), target: "s6.html" },
+];
